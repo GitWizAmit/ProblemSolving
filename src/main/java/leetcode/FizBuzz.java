bfcbc077b9460192ee9c9261897b40bc9ccb074e
amitmahato <amit.mahato1910@gmail.com>
2024-09-06 16:04:10 +0530

    LEETCODE: Fizz Buzz Multithreaded

---
+++ src/main/java/leetcode/FizBuzz.java
@@ -0,0 +1,64 @@
+package main.java.leetcode;
+
+import java.util.function.IntConsumer;
+
+class FizzBuzz {
+    private final int n;
+    private int currentNumber = 1;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public synchronized void fizz(Runnable printFizz) throws InterruptedException {
+        while (currentNumber <= n) {
+            if (currentNumber % 3 != 0 || currentNumber % 5 == 0) {
+                wait();
+                continue;
+            }
+            printFizz.run();
+            currentNumber++;
+            notifyAll();
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public synchronized void buzz(Runnable printBuzz) throws InterruptedException {
+        while (currentNumber <= n) {
+            if (currentNumber % 3 == 0 || currentNumber % 5 != 0) {
+                wait();
+                continue;
+            }
+            printBuzz.run();
+            currentNumber++;
+            notifyAll();
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public synchronized void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        while (currentNumber <= n) {
+            if (currentNumber % 15 != 0) {
+                wait();
+                continue;
+            }
+            printFizzBuzz.run();
+            currentNumber++;
+            notifyAll();
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public synchronized void number(IntConsumer printNumber) throws InterruptedException {
+        while (currentNumber <= n) {
+            if (currentNumber % 3 == 0 || currentNumber % 5 == 0) {
+                wait();
+                continue;
+            }
+            printNumber.accept(currentNumber);
+            currentNumber++;
+            notifyAll();
+        }
+    }
+}
