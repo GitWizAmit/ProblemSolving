7773f8c37ff0d29c7ce7af8d44b5def2189a1fee
amitmahato <amit.mahato1910@gmail.com>
2025-03-25 09:16:50 +0530

    compress string using stack

--- src/main/java/OA/meesho/CompressString.java
+++ src/main/java/OA/meesho/CompressString.java
@@ -3,6 +3,7 @@ package main.java.OA.meesho;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class CompressString {
     public static void main(String[] args) throws IOException {
@@ -10,7 +11,10 @@ public class CompressString {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         int k = Integer.parseInt(br.readLine());
+        // using recursion
         System.out.println(compress(new StringBuilder(s), k, 1));
+        // using stack
+        System.out.println(compress(s, k));
     }
     public static String compress(StringBuilder sb, int k, int count) {
         for (int i = 0; i < sb.length(); i++) {
@@ -25,5 +29,38 @@ public class CompressString {
             }
         }
         return sb.toString();
+    }
+    public static String compress(String sb, int k) {
+        Stack<Pair> stack = new Stack<>();
+
+        for (int i = 0; i < sb.length(); i++) {
+            char c = sb.charAt(i);
+            if (stack.isEmpty() || stack.peek().c != c) {
+                stack.push(new Pair(c, 1));
+            } else {
+                Pair pair = stack.pop();
+                pair.count++;
+                if (pair.count != k) {
+                    stack.push(pair);
+                }
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            Pair pair = stack.pop();
+            result.append(String.valueOf(pair.c).repeat(Math.max(0, pair.count)));
+        }
+
+        return result.reverse().toString();
+
+    }
+    static class Pair {
+        char c;
+        int count;
+        Pair(char c, int count) {
+            this.c = c;
+            this.count = count;
+        }
     }
 }
